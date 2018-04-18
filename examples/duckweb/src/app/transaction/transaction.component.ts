1cd9e45bd33bb6a1d714053e0019698217d29f96
CHARLES EDOU NZE <charles@charlesen.fr>
2018-04-18 21:58:00 +0200

    Track 33 files into repository.
    - untracked examples/duckweb/.angular-cli.json
    - untracked examples/duckweb/.editorconfig
    - untracked examples/duckweb/.gitignore
    - untracked examples/duckweb/README.md
    - untracked examples/duckweb/e2e/app.e2e-spec.ts
    - untracked examples/duckweb/e2e/app.po.ts
    - untracked examples/duckweb/e2e/tsconfig.e2e.json
    - untracked examples/duckweb/karma.conf.js
    - untracked examples/duckweb/package.json
    - untracked examples/duckweb/protractor.conf.js
    - untracked examples/duckweb/src/app/app.component.css
    - untracked examples/duckweb/src/app/app.component.html
    - untracked examples/duckweb/src/app/app.component.spec.ts
    - untracked examples/duckweb/src/app/app.component.ts
    - untracked examples/duckweb/src/app/app.module.ts
    - untracked examples/duckweb/src/app/transaction/transaction.component.css
    - untracked examples/duckweb/src/app/transaction/transaction.component.html
    - untracked examples/duckweb/src/app/transaction/transaction.component.spec.ts
    - untracked examples/duckweb/src/app/transaction/transaction.component.ts
    - untracked examples/duckweb/src/assets/.gitkeep
    - untracked examples/duckweb/src/environments/environment.prod.ts
    - untracked examples/duckweb/src/environments/environment.ts
    - untracked examples/duckweb/src/favicon.ico
    - untracked examples/duckweb/src/index.html
    - untracked examples/duckweb/src/main.ts
    - untracked examples/duckweb/src/polyfills.ts
    - untracked examples/duckweb/src/styles.css
    - untracked examples/duckweb/src/test.ts
    - untracked examples/duckweb/src/tsconfig.app.json
    - untracked examples/duckweb/src/tsconfig.spec.json
    - untracked examples/duckweb/src/typings.d.ts
    - untracked examples/duckweb/tsconfig.json
    - untracked examples/duckweb/tslint.json
    
    Auto commit by GitBook Editor

---
+++ examples/duckweb/src/app/transaction/transaction.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl='https://duckcoin.charlesen.fr'
+
+@Component({
+  selector: 'app-transaction',
+  templateUrl: './transaction.component.html',
+  styleUrls: ['./transaction.component.css']
+})
+export class TransactionComponent implements OnInit {
+  transactions: any = [];
+  constructor(private http: HttpClient) {
+    //
+    this.http.get(`${apiUrl}/transactions`).subscribe(data => function(){
+      console.log('transactions::data', data);
+    });
+    // this.transactions = [
+      {
+        'sender': 'charles',
+        'recipient': 'maxime',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'raphael',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'doreen',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'louis-joseph',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'elise',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'germain',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'anthony',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'pol',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'vincent',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'nicolas',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'kevin',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'willy',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'elodie',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'adrien',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'romain',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'quentin',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'jean-etienne',
+        'amount': 100,
+      },
+      {
+        'sender': 'charles',
+        'recipient': 'gael',
+        'amount': 100,
+      }
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
