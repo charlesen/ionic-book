ddf17585125e8b7b11de45acca0975f33f99c994
ch@rlesen <contact@doreto.fr>
2018-04-20 09:14:57 +0200

    Maj gitignore _book

--- _book/chap-12-introduction-au-pwa.md
+++
@@ -1,4 +0,0 @@
-# Chap 12 - Introduction au PWA avec Stencil et Capacitor
-
-
-
--- _book/examples/demo_typescript.js
+++
@@ -1,5 +0,0 @@
-function ditBonjour(person) {
-    return "Bonjour, " + person;
-}
-var user = "Raphael";
-document.body.innerHTML = ditBonjour(user);
